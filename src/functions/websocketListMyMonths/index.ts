84f070a220c4dac80ea399ba8eff654991d64946
Joan Gerard <joan.gerard@outlook.com>
2022-11-07 17:27:21 +0100

    modify message type for websocket.send

--- src/functions/websocketListMyMonths/index.ts
+++ src/functions/websocketListMyMonths/index.ts
@@ -41,7 +41,7 @@ export const handler = async (event: APIGatewayProxyEvent) => {
       stage,
       message: JSON.stringify({
         data: responseData,
-        type: 'groupData'
+        type: 'monthData'
       }),
     });
 
